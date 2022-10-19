f22af6123ad4838eb19bd1427def21fa38629b7d
holidaez <holidaez@DESKTOP-GG36629.localdomain>
2022-10-19 13:45:11 -0400

    WorkingCode2

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+//!TEST ROUTE
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
 
 module.exports = router;
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
