128dc1c9f33ac832199c9c216808f1e77a2d79b3
holidaez <holidaez@DESKTOP-GG36629.localdomain>
2022-10-21 17:58:59 -0400

    Add user input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,6 +5,21 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+//TODO Restores a session for a user
 router.get(
     '/',
     restoreUser,
@@ -17,30 +32,33 @@ router.get(
       } else return res.json({});
     }
   );
-  
+  //TODO END SESSION RESTORATION
+//! LOGIN ROUTE
 router.post(
-    '/',
-    async (req, res, next) => {
-      const { credential, password } = req.body;
+  '/',
+  validateLogin,
+  async (req, res, next) => {
+    const { credential, password } = req.body;
 
-      const user = await User.login({ credential, password });
+    const user = await User.login({ credential, password });
 
-      if (!user) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = ['The provided credentials were invalid.'];
-        return next(err);
-      }
-
-      await setTokenCookie(res, user);
-
-      return res.json({
-        user
-      });
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
     }
-  );
 
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user
+    });
+  }
+);
+//! END LOGIN ROUTE
+//? LOGOUT ROUTE
   //Removes the token cookie
   router.delete(
     '/',
@@ -49,7 +67,7 @@ router.post(
       return res.json({ message: 'success' });
     }
   );
-
+//? END LOGOUT ROUTE
 
 
 
