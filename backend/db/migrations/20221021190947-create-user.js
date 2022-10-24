f6c1d4c73bfbee3b5f8cea275f1fd34d61fd00e1
holidaez <holidaez@DESKTOP-GG36629.localdomain>
2022-10-24 16:01:05 -0400

    users table fixed

--- backend/db/migrations/20221021190947-create-user.js
+++ backend/db/migrations/20221021190947-create-user.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      firsName:{
+      firstName:{
         type: Sequelize.STRING(30),
         allowNull: false
       },
