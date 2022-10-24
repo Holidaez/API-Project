307153a73b6a9ad0ed7237faa2eb5a59b8b75590
holidaez <holidaez@DESKTOP-GG36629.localdomain>
2022-10-24 15:50:13 -0400

    test

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -18,42 +18,6 @@ router.post('/test', (req, res) => {
 
 
 
-//! Phase 3 Start
 
-
-// router.get(
-//   '/restore-user',
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-
-// //! Auth Testing
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-// router.get('/set-token-cookie', async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: 'Demo-lition'
-//     }
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// });
-
-
-// router.use(restoreUser);
-
-// // ...
-
-// // GET /api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
 //! Phase 3 End
 module.exports = router;
